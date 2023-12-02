10e543c819cb32954d02d585f25b627268b1299b
kirito <hmo825@yahoo.com>
2023-12-02 02:52:35 -0800

    update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_lest = []
     for i in range(len(my_list)):
-        if my_list[i] == 0:
+        if my_list[i]% 2 == 0:
             new_lest.append(True)
         else:
             new_lest.append(False)
